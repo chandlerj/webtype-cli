35fd98a2945c5a0c538ea7e79d4182d07de8efe3
Chandler Justice <chandler.justice@aggiemail.usu.edu>
2020-05-27 13:14:06 -0600

    writing website scraper

--- src/getArticle.py
+++ src/getArticle.py
@@ -1,2 +1,20 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
 class getArticle:
-    pass
+    def __init__(self, webPage):
+        self.webPage = webPage
+
+    def extract(self):
+        content = urllib.request.urlopen(self.webPage)
+
+        readPage = content.read
+        soup = BeautifulSoup(readPage, 'html.parser')
+
+        paragraphs = soup.find_all('p')
+
+        return paragraphs
+
+test = getArticle("http://example.com/")
+
+test.extract()
